2ae5098dd6a94ebe64016ceb2fe0fb7e349da0ed
steeve0403 <zychsteeve4@gmail.com>
2024-08-31 11:53:28 +0200

    New app : auth

--- api_pizza_django/settings.py
+++ api_pizza_django/settings.py
@@ -42,8 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',
     'ninja',
+    'api',
+    'auth',
 ]
 
 MIDDLEWARE = [
